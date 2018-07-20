a5cac43eb8db13f6b6fa4c922048608a22b06b7d
Jak <jak.at@laposte.net>
2018-07-20 11:38:21 +0200

    Je suis trop fort

---
+++ GitPourRepo2/GitPourRepo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPourRepo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo de Git avec repo2 juste pour voir");
+            Console.Read();
+        }
+    }
+}
